f1b31ffffb7467a02dbf11b3c24499c509dfbb29
baxronovferuz1 <baxronovvferuz25@gmail.com>
2024-06-10 18:28:05 +0500

    added Product model

--- delivery_project/models.py
+++ delivery_project/models.py
@@ -13,3 +13,39 @@ class User(Base):
     password=Column(Text, nullable=True)
     is_staff=Column(Boolean, default=False)
     is_active=Column(Boolean, default=False)
+    order=relationship('Order', back_populates='user')
+
+    def __repr__(self):
+        return f"<user {self.id}"
+
+
+
+class Order(Base):
+    ORDER_STATUS=(
+        ('PENDING', 'pending'),
+        ('IN_TRANSIT', 'in_transit'),
+        ('DELIVERED', 'delivered')
+    )
+    __tablename__="orders"
+    id=Column(Integer, primary_key=True)
+    quantity=Column(Integer, nullable=False)
+    order_status=Column(ChoiceType(choices=ORDER_STATUS), default='PENDING')
+    user_id=Column(Integer, ForeignKey('user.id'))
+    user=relationship('User', back_populates='orders')
+    product_id=Column(Integer, ForeignKey('product.id'))
+    product=relationship('Product', back_populates='orders')
+
+    def __repr__(self):
+        return f"<order {self.id}"
+
+
+
+class Product(Base):
+    __tablename__='product'
+    id=Column(Integer, primary_key=True)
+    name=Column(String(100))
+    price=Column(Integer)
+    orders=relationship('Order', back_populates='user')
+
+    def __repr__(self):
+        return f"<product {self.name}"
