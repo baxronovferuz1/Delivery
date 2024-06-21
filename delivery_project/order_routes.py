91307aa9fad130c6655c08d422d2d109efba7d1c
baxronovferuz1 <baxronovvferuz25@gmail.com>
2024-06-21 17:12:40 +0500

    changed order_routes

--- delivery_project/order_routes.py
+++ delivery_project/order_routes.py
@@ -1,10 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends,status
+from fastapi_jwt_auth import AuthJWT
+from models import User,Product,Order
+from schemas import OrderModel,OrderStatusModel
+from database import session,engine
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import HTTPException
 
 order_router = APIRouter(
     prefix="/order",
 )
 
 
+
 @order_router.get("/")
-async def order():
-    return {"message": "delivery page"}
+async def welcome_page(Authorize: AuthJWT=Depends()):
+
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Enter valid access token")  
+
+    return {
+        "message":"Bu order route sahifasi"
+    }  
