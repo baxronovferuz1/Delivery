7abd584be71cec1c91dbf082e50924e98cc2abef
baxronovferuz1 <baxronovvferuz25@gmail.com>
2024-06-21 18:09:55 +0500

    added make_order in order_routes

--- delivery_project/order_routes.py
+++ delivery_project/order_routes.py
@@ -23,3 +23,30 @@ async def welcome_page(Authorize: AuthJWT=Depends()):
     return {
         "message":"Bu order route sahifasi"
     }  
+
+
+@order_router.post('/make', status_code=status.HTTP_201_CREATED)
+async def make_order(order:OrderModel, Authorize:AuthJWT=Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Enter valid access token")
+    
+    current_user=Authorize.get_jwt_subject()
+    user=session.query(User).filter(User.username==current_user).first()
+
+    new_order=Order(
+        quantity=order.quantity,
+        #product=order.product_id
+    )
+    new_order.user=user
+    session.add(new_order)
+    session.commit()
+
+    response={
+        "id":new_order.id,
+        "quantity":new_order.quantity,
+        "order_statuses":new_order.order_status,
+    }
+
+    return jsonable_encoder(response)
